ef38aaa3487dcf75a6a8e5c2c0ce3931e9dee6a1
Restyled.io <commits@restyled.io>
2020-02-13 00:53:13 +0000

    Restyled by clang-format

--- js/entity/SignedOrderEntity.js
+++ js/entity/SignedOrderEntity.js
@@ -1,65 +1,65 @@
 'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+Object.defineProperty(exports, '__esModule', {value : true});
 var typeorm_1 = require('typeorm');
 var SignedOrderModel_1 = require('../models/SignedOrderModel');
 exports.signedOrderEntity = new typeorm_1.EntitySchema({
-    name: 'SignedOrder',
-    target: SignedOrderModel_1.SignedOrderModel,
-    columns: {
-        hash: {
-            primary: true,
-            type: 'varchar',
-        },
-        senderAddress: {
-            type: 'varchar',
-        },
-        makerAddress: {
-            type: 'varchar',
-        },
-        takerAddress: {
-            type: 'varchar',
-        },
-        makerAssetData: {
-            type: 'varchar',
-        },
-        takerAssetData: {
-            type: 'varchar',
-        },
-        exchangeAddress: {
-            type: 'varchar',
-        },
-        feeRecipientAddress: {
-            type: 'varchar',
-        },
-        expirationTimeSeconds: {
-            type: 'varchar',
-        },
-        makerFee: {
-            type: 'varchar',
-        },
-        takerFee: {
-            type: 'varchar',
-        },
-        makerFeeAssetData: {
-            type: 'varchar',
-        },
-        takerFeeAssetData: {
-            type: 'varchar',
-        },
-        makerAssetAmount: {
-            type: 'varchar',
-        },
-        takerAssetAmount: {
-            type: 'varchar',
-        },
-        salt: {
-            type: 'varchar',
-        },
-        signature: {
-            type: 'varchar',
-        },
-        remainingFillableTakerAssetAmount: {
-            type: 'varchar',
-        },
+  name : 'SignedOrder',
+  target : SignedOrderModel_1.SignedOrderModel,
+  columns : {
+    hash : {
+      primary : true,
+      type : 'varchar',
     },
+    senderAddress : {
+      type : 'varchar',
+    },
+    makerAddress : {
+      type : 'varchar',
+    },
+    takerAddress : {
+      type : 'varchar',
+    },
+    makerAssetData : {
+      type : 'varchar',
+    },
+    takerAssetData : {
+      type : 'varchar',
+    },
+    exchangeAddress : {
+      type : 'varchar',
+    },
+    feeRecipientAddress : {
+      type : 'varchar',
+    },
+    expirationTimeSeconds : {
+      type : 'varchar',
+    },
+    makerFee : {
+      type : 'varchar',
+    },
+    takerFee : {
+      type : 'varchar',
+    },
+    makerFeeAssetData : {
+      type : 'varchar',
+    },
+    takerFeeAssetData : {
+      type : 'varchar',
+    },
+    makerAssetAmount : {
+      type : 'varchar',
+    },
+    takerAssetAmount : {
+      type : 'varchar',
+    },
+    salt : {
+      type : 'varchar',
+    },
+    signature : {
+      type : 'varchar',
+    },
+    remainingFillableTakerAssetAmount : {
+      type : 'varchar',
+    },
+  },
 });
